a371058bfdd5afa2e93d51a7d745799d605be75a
Paul Val <paul.val1@hotmail.com>
2019-07-17 19:50:52 +0100

    elementary button click action

--- booking_form.js
+++ booking_form.js
@@ -9,3 +9,25 @@ Set values of:
     data-count, textContent of #count, input[price-input]-value, input[submit]-value
 
 */
+
+const getCurrentData = e => {
+  let offset = e.currentTarget.dataset.offset;
+  offset = parseInt(offset, 10);
+  console.log(offset);
+
+  let priceCents = document.querySelector('#price').dataset.amountCents;
+  priceCents = parseInt(priceCents, 10);
+  console.log(priceCents);
+};
+
+const setEventListener = button => {
+  button.addEventListener('click', e => {
+    // console.log(e);
+    getCurrentData(e);
+  });
+};
+
+const buttons = document.querySelectorAll('.incrementer');
+buttons.forEach(button => {
+  setEventListener(button);
+});
