21213624c446c2e15d600b29ee46b1f34ee438e2
Paul Val <paul.val1@hotmail.com>
2019-07-17 19:20:59 +0100

    initial commit

---
+++ booking_form.js
@@ -0,0 +1,11 @@
+/*
+For this we need to grab the + & - buttons and set an event listener on them
+Then when a button is clicked we get the current values of:
+    data-offset 
+    data-amount-cents
+    data-count
+    textContent of #count
+Set values of:
+    data-count, textContent of #count, input[price-input]-value, input[submit]-value
+
+*/
